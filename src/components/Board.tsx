f92323ec26ba2623224ac03af574e6a08fcc0a3c
brazillier_thi <thibault.brazillier@supelec.fr>
2020-09-09 16:31:06 +0200

    feat(status): add complete and start button

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,12 +5,15 @@ import {
 	Typography,
 	CircularProgress,
 	Grid,
+	Button,
+	CardActions,
 } from "@material-ui/core";
 
 import { useRealmApp } from "../realm/RealmApp";
 import {
 	useGetAllKDramasQuery,
 	useAddKDramaMutation,
+	useSetKDramaStatusMutation,
 } from "../graphql-operations";
 import { GetAllKDramasQuery, KDrama, KDramaInsertInput } from "../types";
 import { STATUSES } from "../Constants";
@@ -21,9 +24,11 @@ import AddKDramaForm from "./AddKDramaForm";
 const Board: React.FC = () => {
 	const [kDramas, setKDramas] = useState<KDrama[]>([]);
 	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [statusFilter, setStatusFilter] = useState<string | null>(null);
 
 	const { logOut } = useRealmApp();
 	const [addKDramaMutation] = useAddKDramaMutation();
+	const [setKDramaStatusMutation] = useSetKDramaStatusMutation();
 	const { loading } = useGetAllKDramasQuery({
 		onCompleted: (data: GetAllKDramasQuery) => {
 			if (data?.kDramas) {
@@ -44,12 +49,33 @@ const Board: React.FC = () => {
 		}
 	};
 
+	const setStatus = async (id: string, status: string) => {
+		const currentKDramas = [...kDramas];
+		try {
+			const result = await setKDramaStatusMutation({
+				variables: { id, status },
+			});
+			console.log(result);
+			const updatedKDrama = result.data?.kDrama as KDrama;
+			currentKDramas[
+				currentKDramas.findIndex(({ _id }) => _id === updatedKDrama._id)
+			] = updatedKDrama;
+			setKDramas(currentKDramas);
+		} catch (err) {
+			setKDramas(currentKDramas);
+			throw new Error("Couldn't set status");
+		}
+	};
+
 	const renderCardContent = () => {
 		if (loading) return <CircularProgress />;
 
 		const currentKDrama = kDramas.find(
-			({ status }) => status === STATUSES.WATCHING
+			({ status }) =>
+				status === STATUSES.WATCHING || status === STATUSES.PLANNED
 		);
+
+		const isWatching = currentKDrama?.status === STATUSES.WATCHING;
 
 		return (
 			<CardContent style={{ padding: "0px" }}>
@@ -57,11 +83,25 @@ const Board: React.FC = () => {
 					<img width="800px" src={currentKDrama.image} alt="kdrama" />
 				)}
 				<Typography style={{ paddingLeft: "5px" }} variant="h4">
-					Currently watching:
+					{isWatching ? "Currently watching:" : "Next planned:"}
 				</Typography>
 				<Typography style={{ paddingLeft: "5px" }} variant="h3">
 					{currentKDrama?.title ?? "No currently watched KDrama"}
 				</Typography>
+				<CardActions>
+					<Button
+						variant="outlined"
+						color="primary"
+						onClick={() =>
+							setStatus(
+								currentKDrama?._id,
+								isWatching ? STATUSES.COMPLETED : STATUSES.WATCHING
+							)
+						}
+					>
+						{isWatching ? "Complete" : "Start Watching"}
+					</Button>
+				</CardActions>
 			</CardContent>
 		);
 	};
@@ -83,12 +123,18 @@ const Board: React.FC = () => {
 			</Card>
 			<KDramaList
 				list={kDramas
-					.filter(({ status }) => status !== STATUSES.WATCHING)
+					.filter(({ status }) =>
+						statusFilter ? status === statusFilter : true
+					)
 					.slice(0, 4)}
 				isLoading={loading}
 			/>
 			<FabGroup handleOpen={() => setIsOpen(true)} logOut={logOut} />
-			<AddKDramaForm addKDrama={handleSubmit} handleClose={() => setIsOpen(false)} isOpen={isOpen} />
+			<AddKDramaForm
+				addKDrama={handleSubmit}
+				handleClose={() => setIsOpen(false)}
+				isOpen={isOpen}
+			/>
 		</Grid>
 	);
 };
