32c1812144372f39182758ee3499fe5c786a9351
brazillier_thi <thibault.brazillier@supelec.fr>
2020-09-09 09:19:33 +0200

    Updates

---
+++ precache-manifest.a30b9d1c0f966688848361e4336873c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79031c637229686485c8e2a587a10667",
+    "url": "/my-kdrama-list/index.html"
+  },
+  {
+    "revision": "2d1298008aaff9cd0a45",
+    "url": "/my-kdrama-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "17d5d86a1cbaf7fae691",
+    "url": "/my-kdrama-list/static/js/2.81a15587.chunk.js"
+  },
+  {
+    "revision": "1e969f6aeaeb3ba554c0bba252bfb22c",
+    "url": "/my-kdrama-list/static/js/2.81a15587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1298008aaff9cd0a45",
+    "url": "/my-kdrama-list/static/js/main.6075bc85.chunk.js"
+  },
+  {
+    "revision": "f456a9fb6442766a47c8",
+    "url": "/my-kdrama-list/static/js/runtime-main.172adaf8.js"
+  }
+]);
