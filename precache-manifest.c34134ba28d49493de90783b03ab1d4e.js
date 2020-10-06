8c29ea6cef2c0d2d3bf1decd7748ddd81771b003
brazillier_thi <thibault.brazillier@supelec.fr>
2020-10-06 09:51:29 +0200

    Updates

--- precache-manifest.c34134ba28d49493de90783b03ab1d4e.js
+++ precache-manifest.c34134ba28d49493de90783b03ab1d4e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1140a7417a92123b3e8e37404f223abe",
+    "revision": "97a6aa765ab3a8e008e4f9078d7212bd",
     "url": "/my-kdrama-list/index.html"
   },
   {
-    "revision": "ebd6d0c295ddd58ccab5",
+    "revision": "333bb8613803e9a57e2d",
     "url": "/my-kdrama-list/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "7c0618934e07768673f8",
-    "url": "/my-kdrama-list/static/js/2.70844c21.chunk.js"
+    "revision": "98f39989230094dc54cc",
+    "url": "/my-kdrama-list/static/js/2.8ecd8d3c.chunk.js"
   },
   {
     "revision": "1e969f6aeaeb3ba554c0bba252bfb22c",
-    "url": "/my-kdrama-list/static/js/2.70844c21.chunk.js.LICENSE.txt"
+    "url": "/my-kdrama-list/static/js/2.8ecd8d3c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ebd6d0c295ddd58ccab5",
-    "url": "/my-kdrama-list/static/js/main.e9460a08.chunk.js"
+    "revision": "333bb8613803e9a57e2d",
+    "url": "/my-kdrama-list/static/js/main.f5654ba0.chunk.js"
   },
   {
     "revision": "f456a9fb6442766a47c8",
