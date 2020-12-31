dd3aa8ff20584191d979fa86b0b2108368fd350b
Thibault Brazillier <thbhcm@gmail.com>
2020-12-31 09:04:40 +0100

    fix(constants): single export

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,17 +1,19 @@
-export const STATUSES = {
-	WATCHING: "watching",
-	COMPLETED: "completed",
-	PLANNED: "planned",
+const STATUSES = {
+  WATCHING: "watching",
+  COMPLETED: "completed",
+  PLANNED: "planned",
 };
 
-export const ACTIONS_TEXT: { [key: string]: string } = {
-	watching: "Complete",
-	completed: "Completed",
-	planned: "Start Watching",
+const ACTIONS_TEXT: { [key: string]: string } = {
+  watching: "Complete",
+  completed: "Completed",
+  planned: "Start Watching",
 };
 
-export const STATUS_TEXT: { [key: string]: string } = {
-	watching: "Currently watching",
-	completed: "Completed",
-	planned: "Next planned"
-}
+const STATUS_TEXT: { [key: string]: string } = {
+  watching: "Currently watching",
+  completed: "Completed",
+  planned: "Next planned",
+};
+
+export { STATUSES, ACTIONS_TEXT, STATUS_TEXT };
