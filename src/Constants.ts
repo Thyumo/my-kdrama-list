7bb0e2c062b73d5278fb53629830d921d89fe5ce
Thibault Brazillier <thbhcm@gmail.com>
2021-08-14 02:02:24 +0200

    feat(statuses): add hidden status

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,18 +2,21 @@ const STATUSES = {
   WATCHING: "watching",
   COMPLETED: "completed",
   PLANNED: "planned",
+  HIDDEN: "hidden"
 };
 
 const ACTIONS_TEXT: { [status: string]: string } = {
   watching: "Complete",
   completed: "Completed",
   planned: "Start Watching",
+  hidden: "Hidden"
 };
 
 const STATUS_TEXT: { [status: string]: string } = {
   watching: "Currently watching",
   completed: "Completed",
   planned: "Next planned",
+  hidden: "Hidden"
 };
 
 const PAGE_SIZE = 4;
